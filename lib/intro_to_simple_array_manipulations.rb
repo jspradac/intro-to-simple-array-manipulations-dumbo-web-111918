a29024d2fde4cec78a3d3b57390ca5e0e8363045
Samuel Prada <jspradac@gmail.com>
2018-11-06 16:30:41 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,4 +4,17 @@ end
 def using_unshift(array, string)
   array.unshift(string)
 end
-unding
+def using_pop(array)
+  array.pop 
+end 
+def pop_with_args(array)
+  array.pop(2)
+end 
+def using_shift(array)
+  array.shift 
+end 
+def shift_with_args(array)
+  array.shift(2)
+end
+
+  
